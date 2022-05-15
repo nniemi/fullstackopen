1fc702eb569a7953b4e27f4d2bd3a2b665c83211
nniemi <70947133+nniemi@users.noreply.github.com>
2022-05-15 17:43:59 +0300

    Tasks 1.7 & 1.8 done

--- Section 1/Task 1.6/task1.6/src/App.js
+++ Section 1/Task 1.6/task1.6/src/App.js
@@ -1,48 +1,66 @@
 import { useState } from 'react'
 
+
+const Statistics = (props) => {
+  
+  
+  return(
+  <div>
+  <h1>
+    Give feedback
+  </h1>
+  <div>
+    <button onClick={() => props.setGood(props.good + 1, props.allClicks + 1)}>
+      good
+    </button>
+    
+    <button onClick={() => props.setNeutral(props.neutral + 1, props.allClicks + 1)}>
+      neutral
+    </button>
+
+    <button onClick={() => props.setBad(props.bad + 1, props.allClicks + 1)}>
+      bad
+    </button>
+  </div>
+  <h1>
+    Stats
+  </h1>
+  
+  
+    <><div> 
+          good {props.good}
+        </div><div>
+            neutral {props.neutral}
+          </div><div>
+            bad {props.bad}
+          </div><div>
+            all {props.good + props.neutral + props.bad}
+          </div><div>
+            average {(props.good * 1 + props.neutral * 0 + props.bad * (-1)) / (props.good + props.neutral + props.bad)}
+          </div><div>
+            positive {props.good / (props.good + props.neutral + props.bad)}%
+          </div></>
+  
+  </div>
+  
+  )
+
+
+  
+}
+
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  let allClicks = 0;
 
   return (
-    <div>
-      
-      <h1>
-        Give feedback
-      </h1>
-      <div>
-        <button onClick={() => setGood(good + 1)}>
-          good
-        </button>
-        
-        <button onClick={() => setNeutral(neutral + 1)}>
-          neutral
-        </button>
+      <Statistics good = {good} setGood ={setGood} neutral = {neutral} setNeutral = {setNeutral} bad = {bad} setBad = {setBad} allClicks = {allClicks}>
 
-        <button onClick={() => setBad(bad + 1)}>
-          bad
-        </button>
-      </div>
-      <h1>
-        Stats
-      </h1>
-      <div>
-        <p>
-          good {good}
-        </p>
-      </div>
-      <div>
-        <p>
-        neutral {neutral}
-        </p>
-      </div>
-      <div>   
-        bad {bad}
-      </div>
-
-    </div>
+      </Statistics>
   )
 }
 
